627c05ce1fc615241813eee4d68c6a37e28ea377
Ankur Ankan <root@ubuntu.ubuntu-domain>
2012-09-12 12:57:40 -0400

    corrected_fe_user_call.php

--- fe_user_call.php
+++ fe_user_call.php
@@ -1,13 +1,13 @@
 <?php
-require 'map.xml';
+require 'map.php';
 class fe_user_call{
 	function __construct($xmlstr){
 		$xml = new SimpleXMLElement($xmlstr);
 		$fe_user = new fe_users();
 		foreach ($xml->children() as $element){
-			$temp = $element->getName();
-			$func = 'get_'+ $temp;
-			$fe_user->$func($xml->fe_users->$temp);
+			$temp =  $element->getName();
+			$func = 'set_'. $temp;
+			$fe_user->{$func}($element);
 		}
 	}
 }
