ca98fe9aa3aaff066897828b6fa01786e6406ee3
Ankur Ankan <root@ubuntu.ubuntu-domain>
2012-09-16 16:34:11 -0400

    using-get-set-methods

--- fe_user_call.php
+++ fe_user_call.php
@@ -1,13 +1,15 @@
 <?php
 require 'map.php';
 class fe_user_call{
+	public $fe_user;
 	function __construct($xmlstr){
 		$xml = new SimpleXMLElement($xmlstr);
 		$fe_user = new fe_users();
-		foreach ($xml->children() as $element){
-			$temp =  $element->getName();
-			$func = 'set_'. $temp;
-			$fe_user->{$func}($element);
+		foreach ($xml->children() as $value){
+			$name =  $element->getName();
+			$fe_user->$name = $value;
+			//$func = 'set_'. $temp;
+			//$fe_user->{$func}($element);
 		}
 	}
 }
